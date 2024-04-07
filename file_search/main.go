8d9223e09a0d89e9314d1ac42f0e5ab7dbbb12cf
ae <adamerikoff@protonmail.com>
2024-04-07 19:44:18 +0500

    Update main.go

--- file_search/main.go
+++ file_search/main.go
@@ -5,29 +5,57 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 )
 
 var (
-	matches []string
+	matches   []string
+	waitGroup = sync.WaitGroup{}
+	lock      = sync.Mutex{}
 )
 
-func fileSearch(root string, filename string) {
-	fmt.Println("Searching in", root)
+func fileSearch(root string, filename string, interactive bool) {
+	if interactive {
+		fmt.Println("Searching in", root)
+	}
 	files, _ := os.ReadDir(root)
 	for _, file := range files {
 		if strings.Contains(file.Name(), filename) {
+			lock.Lock()
 			matches = append(matches, filepath.Join(root, filename))
+			lock.Unlock()
 		}
 		if file.IsDir() {
-			fileSearch(filepath.Join(root, file.Name()), filename)
+			waitGroup.Add(1)
+			go fileSearch(filepath.Join(root, file.Name()), filename, interactive)
+
 		}
 	}
+	waitGroup.Done()
 }
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run main.go <directory> <filename> <flags>")
+		return
+	}
+
+	directory := os.Args[1]
+	filename := os.Args[2]
+
+	interactive := false
+
+	if len(os.Args) > 3 && os.Args[3] == "i" {
+		interactive = true
+	}
+
 	startTime := time.Now()
-	fileSearch("/Users/adamerik/Documents/code/python/ml_algorithms", "voting.ipynb")
+
+	waitGroup.Add(1)
+	go fileSearch(directory, filename, interactive)
+	waitGroup.Wait()
+
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
 	for _, file := range matches {
