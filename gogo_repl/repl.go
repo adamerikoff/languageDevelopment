dc27a88a04be18f71a11e2005350e3801d7d50c5
Adam Erikuly <adamerikoff@protonmail.com>
2024-06-09 21:50:59 +0500

    basec REPL implemented

--- gogo_repl/repl.go
+++ gogo_repl/repl.go
@@ -1,1 +1,30 @@
 package gogo_repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/adamerikoff/gogo/gogo_lexer"
+	"github.com/adamerikoff/gogo/gogo_token"
+)
+
+const PROMPT = "=>"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprint(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := gogo_lexer.NewLexer(line)
+
+		for tok := l.NextToken(); tok.Type != gogo_token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "| Type: %s, Literal: %s |\n", tok.Type, tok.Literal)
+		}
+	}
+}
