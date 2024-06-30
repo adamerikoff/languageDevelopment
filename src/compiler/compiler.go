e036c7f467d37e16c77dfea511b0644fd24860dd
Adam Erikuly <adamerikoff@protonmail.com>
2024-06-30 21:18:57 +0500

    basic compiler for addition arithmetic implemented

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -1,6 +1,8 @@
 package compiler
 
 import (
+	"fmt"
+
 	"github.com/adamerikoff/ponGo/src/ast"
 	"github.com/adamerikoff/ponGo/src/code"
 	"github.com/adamerikoff/ponGo/src/object"
@@ -42,6 +44,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		err = c.Compile(node.Right)
 		if err != nil {
 			return err
+		}
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 
 	case *ast.IntegerLiteral:
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,47 +5,47 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/adamerikoff/ponGo/src/evaluator"
+	"github.com/adamerikoff/ponGo/src/compiler"
 	"github.com/adamerikoff/ponGo/src/lexer"
-	"github.com/adamerikoff/ponGo/src/object"
 	"github.com/adamerikoff/ponGo/src/parser"
+	"github.com/adamerikoff/ponGo/src/vm"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-	macroEnv := object.NewEnvironment()
+// func Start(in io.Reader, out io.Writer) {
+// 	scanner := bufio.NewScanner(in)
+// 	env := object.NewEnvironment()
+// 	macroEnv := object.NewEnvironment()
 
-	for {
-		fmt.Fprintf(out, PROMPT)
-		scanned := scanner.Scan()
-		if !scanned {
-			return
-		}
+// 	for {
+// 		fmt.Fprintf(out, PROMPT)
+// 		scanned := scanner.Scan()
+// 		if !scanned {
+// 			return
+// 		}
 
-		line := scanner.Text()
-		l := lexer.New(line)
-		p := parser.New(l)
+// 		line := scanner.Text()
+// 		l := lexer.New(line)
+// 		p := parser.New(l)
 
-		program := p.ParseProgram()
-		if len(p.Errors()) != 0 {
-			printParserErrors(out, p.Errors())
-			continue
-		}
+// 		program := p.ParseProgram()
+// 		if len(p.Errors()) != 0 {
+// 			printParserErrors(out, p.Errors())
+// 			continue
+// 		}
 
-		evaluator.DefineMacros(program, macroEnv)
-		expanded := evaluator.ExpandMacros(program, macroEnv)
+// 		evaluator.DefineMacros(program, macroEnv)
+// 		expanded := evaluator.ExpandMacros(program, macroEnv)
 
-		evaluated := evaluator.Eval(expanded, env)
+// 		evaluated := evaluator.Eval(expanded, env)
 
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
-		}
-	}
-}
+// 		if evaluated != nil {
+// 			io.WriteString(out, evaluated.Inspect())
+// 			io.WriteString(out, "\n")
+// 		}
+// 	}
+// }
 
 const MONKEY_FACE = `            __,__
    .--.  .-"     "-.  .--.
@@ -66,5 +66,39 @@ func printParserErrors(out io.Writer, errors []string) {
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+		comp := compiler.New()
+		err := comp.Compile(program)
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
+			continue
+		}
+		machine := vm.New(comp.Bytecode())
+		err = machine.Run()
+		if err != nil {
+			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
+			continue
+		}
+		stackTop := machine.StackTop()
+		io.WriteString(out, stackTop.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
