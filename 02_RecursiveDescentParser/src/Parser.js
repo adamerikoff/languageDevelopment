cee9558aeb956e833771c6e7475bb81cfac7602c
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-09 19:09:23 +0500

    init: RecursiveDescentParser started

---
+++ 02_RecursiveDescentParser/src/Parser.js
@@ -0,0 +1,25 @@
+class Parser {
+
+    parse(string) {
+        this._string = string;
+        return this.Program();
+    }
+
+    Program() {
+        return {
+            type: "Program",
+            body: this.NumericLiteral(),
+        }
+    }
+
+    NumericLiteral() {
+        return {
+            type: "NumericLiteral",
+            value: Number(this._string),
+        }
+    }
+}
+
+module.exports = {
+    Parser,
+}
