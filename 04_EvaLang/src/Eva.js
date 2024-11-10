e3a3aefe0a7c956f7d7013e6a7aa7f51bf50784b
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-10 05:53:35 +0500

    feat: Integers and Strings implemented

--- 04_EvaLang/src/Eva.js
+++ 04_EvaLang/src/Eva.js
@@ -1,6 +1,20 @@
 class Eva {
     eval(exp) {
+        if (this.isNumber(exp)) {
+            return exp;
+        }
+        if (this.isString(exp)) {
+            return exp.slice(1, -1);
+        }
         throw "Unimplemented";
+    }
+
+    isNumber(exp) {
+        return typeof exp === "number";
+    }
+
+    isString(exp) {
+        return typeof exp === "string" && exp[0] === `"` && exp.slice(-1) === `"`;
     }
 }
 
--- 04_EvaLang/test/Test.js
+++ 04_EvaLang/test/Test.js
@@ -4,3 +4,6 @@ const { Eva } = require("../src/Eva");
 const eva = new Eva();
 
 assert.strictEqual(eva.eval(1), 1);
+assert.strictEqual(eva.eval(`"hello"`), "hello");
+
+console.log("ALL ASSERTIONS PASSED!");
