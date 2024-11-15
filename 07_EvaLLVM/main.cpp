aafe734cc164fdbaeb84cb84508a87823837b374
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-16 04:49:11 +0500

    feat: i guess smthing generates and working

--- 07_EvaLLVM/main.cpp
+++ 07_EvaLLVM/main.cpp
@@ -4,12 +4,7 @@ int main(int argc, char const* argv[]) {
     std::string program = R"(
         42
     )";
-
     EvaLLVM vm;
-
     vm.exec(program);
-
-    std::cout << "Hello, World!" << std::endl;
-
     return 0;
 }
--- 07_EvaLLVM/src/EvaLLVM.h
+++ 07_EvaLLVM/src/EvaLLVM.h
@@ -13,6 +13,7 @@ private:
     std::unique_ptr<llvm::LLVMContext> ctx;
     std::unique_ptr<llvm::Module> module;
     std::unique_ptr<llvm::IRBuilder<>> builder;
+    llvm::Function* fn;
     void moduleInit() {
         ctx = std::make_unique<llvm::LLVMContext>();
         module = std::make_unique<llvm::Module>("EvaLLVM", *ctx);
@@ -23,6 +24,35 @@ private:
         llvm::raw_fd_ostream ouLL(fileName, errorCode);
         module->print(ouLL, nullptr);
     }
+    void compile() {
+        fn = createFunction("main", llvm::FunctionType::get(builder->getInt32Ty(), false));
+        auto result = gen();
+        auto i32Result = builder->CreateIntCast(result, builder->getInt32Ty(), true);
+        builder->CreateRet(i32Result);
+    }
+    llvm::Value* gen() {
+        return builder->getInt32(42);
+    }
+    llvm::Function* createFunction(const std::string& fnName, llvm::FunctionType* fnType) {
+        auto fn = module->getFunction(fnName);
+        if (fn == nullptr) {
+            fn = createFunctionProto(fnName, fnType);
+        }
+        createFunctionBlock(fn);
+        return fn;
+    }
+    llvm::Function* createFunctionProto(const std::string& fnName, llvm::FunctionType* fnType) {
+        auto fn = llvm::Function::Create(fnType, llvm::Function::ExternalLinkage, fnName, *module);
+        verifyFunction(*fn);
+        return fn;
+    }
+    void createFunctionBlock(llvm::Function* fn) {
+        auto entry = createBB("entry", fn);
+        builder->SetInsertPoint(entry);
+    }
+    llvm::BasicBlock* createBB(std::string name, llvm::Function* fn = nullptr) {
+        return llvm::BasicBlock::Create(*ctx, name, fn);
+    }
 public: 
     EvaLLVM() {
         moduleInit();
@@ -31,6 +61,7 @@ public:
     void exec(const std::string& program) {
         // auto ast = parser->parser(program)
         // compile(ast);
+        compile();
         module->print(llvm::outs(), nullptr);
         saveModuleToFile("./out.ll");
     }
