78d7ef191898f5e99038f8e1d45c4223a6b999e9
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-18 17:49:52 +0500

    feat: control flow is implemented

--- 08_LoxLanguage/src/loxLanguage/Interpreter.java
+++ 08_LoxLanguage/src/loxLanguage/Interpreter.java
@@ -49,6 +49,14 @@ public class Interpreter implements Expr.Visitor<Object>,
 	}
 	
 	@Override
+	public Void visitWhileStmt(Stmt.While stmt) {
+		while (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.body);
+		}
+		return null;
+	}
+	
+	@Override
 	public Object visitAssignExpr(Expr.Assign expr) {
 		Object value = evaluate(expr.value);
 		environment.assign(expr.name, value);
@@ -58,6 +66,17 @@ public class Interpreter implements Expr.Visitor<Object>,
 	@Override
 	public Object visitLiteralExpr(Expr.Literal expr) {
 		return expr.value;
+	}
+	
+	@Override
+	public Object visitLogicalExpr(Expr.Logical expr) {
+		Object left = evaluate(expr.left);
+		if (expr.operator.type == TokenType.OR) {
+			if (isTruthy(left)) return left;
+		} else {
+			if (!isTruthy(left)) return left;
+		}
+		return evaluate(expr.right);
 	}
 	
 	@Override
@@ -132,6 +151,16 @@ public class Interpreter implements Expr.Visitor<Object>,
 	@Override
 	public Void visitExpressionStmt(Stmt.Expression stmt) {
 		evaluate(stmt.expression);
+		return null;
+	}
+	
+	@Override
+	public Void visitIfStmt(Stmt.If stmt) {
+		if (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.thenBranch);
+		} else if (stmt.elseBranch != null) {
+			execute(stmt.elseBranch);
+		}
 		return null;
 	}
 	
--- 08_LoxLanguage/src/loxLanguage/Parser.java
+++ 08_LoxLanguage/src/loxLanguage/Parser.java
@@ -1,6 +1,8 @@
 package loxLanguage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
 import java.util.List;
 
 public class Parser {
@@ -41,9 +43,64 @@ public class Parser {
 	}
 	
 	private Stmt statement() {
+		if (match(TokenType.FOR)) return forStatement();
+		if (match(TokenType.IF)) return ifStatement();
 		if (match(TokenType.PRINT)) return printStatement();
+		if (match(TokenType.WHILE)) return whileStatement();
 		if (match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
 		return expressionStatement();
+	}
+	
+	private Stmt forStatement() {
+		consume(TokenType.LEFT_PARENTHESIS, "Expect '(' after 'for'.");
+		Stmt initializer;
+		if (match(TokenType.SEMICOLON)) {
+			initializer = null;
+		} else if (match(TokenType.VAR)) {
+			initializer = varDeclaration();
+		} else {
+			initializer = expressionStatement();
+		}
+		Expr condition = null;
+		if (!check(TokenType.SEMICOLON)) {
+			condition = expression();
+		}
+		consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+		Expr increment = null;
+		if (!check(TokenType.RIGHT_PARENTHESIS)) {
+		increment = expression();
+		}
+		consume(TokenType.RIGHT_PARENTHESIS, "Expect ')' after for clauses.");
+		Stmt body = statement();
+		if (increment != null) {
+			body = new Stmt.Block(Arrays.asList(body,new Stmt.Expression(increment)));
+		}
+		if (condition == null) condition = new Expr.Literal(true);
+		body = new Stmt.While(condition, body);
+		if (initializer != null) {
+			body = new Stmt.Block(Arrays.asList(initializer, body));
+		}
+		return body;
+	}
+	
+	private Stmt whileStatement() {
+		consume(TokenType.LEFT_PARENTHESIS, "Expect '(' after 'while'.");
+		Expr condition = expression();
+		consume(TokenType.RIGHT_PARENTHESIS, "Expect ')' after condition.");
+		Stmt body = statement();
+		return new Stmt.While(condition, body);
+	}
+	
+	private Stmt ifStatement() {
+		consume(TokenType.LEFT_PARENTHESIS, "Expect '(' after 'if'.");
+		Expr condition = expression();
+		consume(TokenType.RIGHT_PARENTHESIS, "Expect ')' after 'if' condition.");
+		Stmt thenBranch = statement();
+		Stmt elseBranch = null;
+		if (match(TokenType.ELSE)) {
+			elseBranch = statement();
+		}
+		return new Stmt.If(condition, thenBranch, elseBranch);
 	}
 	
 	private List<Stmt> block() {
@@ -78,6 +135,26 @@ public class Parser {
 				return new Expr.Assign(name, value);
 			}
 			error(equals, "Invalid assignment target.");
+		}
+		return expr;
+	}
+	
+	private Expr or() {
+		Expr expr = and();
+		while (match(TokenType.OR)) {
+			Token operator = previous();
+			Expr right = and();
+			expr = new Expr.Logical(expr, operator, right);
+		}
+		return expr;
+	}
+	
+	private Expr and() {
+		Expr expr = equality();
+		while (match(TokenType.AND)) {
+			Token operator = previous();
+			Expr right = equality();
+			expr = new Expr.Logical(expr, operator, right);
 		}
 		return expr;
 	}
