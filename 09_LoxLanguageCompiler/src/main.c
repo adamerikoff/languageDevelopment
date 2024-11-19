d9b936e503f7155a7cdb6ff7920afa595cb5aa28
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-19 05:06:14 +0500

    feat: base code setup

--- 09_LoxLanguageCompiler/src/main.c
+++ 09_LoxLanguageCompiler/src/main.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "./headers/common.h"
+#include "./headers/chunk.h"
+#include "./headers/debug.h"
 
-int main() {
-	printf("HELLO!");
-	return 0;
+int main(int argc, const char* argv[]) {
+    (void)argc;
+    (void)argv;
+
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
 }
