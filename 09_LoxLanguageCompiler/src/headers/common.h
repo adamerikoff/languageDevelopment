1e5403bb730c18fc172bde922f9c4e3230fff996
Adam Erikuly <adamerikoff@protonmail.com>
2024-11-19 08:11:14 +0500

    feat: compiler implemented

--- 09_LoxLanguageCompiler/src/headers/common.h
+++ 09_LoxLanguageCompiler/src/headers/common.h
@@ -1,10 +1,13 @@
 #ifndef CLOX_COMMON_H
 #define CLOX_COMMON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
+
+#define DEBUG_TRACE_EXECUTION
 
 #endif
---
+++ 09_LoxLanguageCompiler/src/headers/compiler.h
@@ -0,0 +1,9 @@
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+#include "common.h"
+#include "scanner.h"
+
+void compile(const char* source);
+
+#endif
